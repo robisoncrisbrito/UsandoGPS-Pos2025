4d5e182dc868f93ec9fedf9f420b55734bc26f54
robisoncrisbrito <robison.brito@gmail.com>
2025-04-26 13:39:56 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/usandogps_pos2025/MapsActivity.kt
@@ -0,0 +1,63 @@
+package br.edu.utfpr.usandogps_pos2025
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import br.edu.utfpr.usandogps_pos2025.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+
+        val latitude = intent.getDoubleExtra( "latitude" , 0.0 )
+        val longitude = intent.getDoubleExtra( "longitude", 0.0  )
+
+        val msgRetorno = recuperaEndereco( latitude, longitude )
+
+        val local = LatLng(latitude, longitude )
+        mMap.addMarker(MarkerOptions().position(local).title( msgRetorno ) )
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(local))
+
+        mMap.setMapType( GoogleMap.MAP_TYPE_SATELLITE )
+    }
+
+    fun recuperaEndereco( latitude : Double, longitude : Double ) : String {
+        val endereco = "https://maps.googleapis.com/maps/api/geocode/xml?latlng=${latitude},${longitude}&key=AIzaSyCMzWccWPPD5Q8mKmyk0AVx3e-_SgTakpA"
+
+
+        return ""
+    }
+}
